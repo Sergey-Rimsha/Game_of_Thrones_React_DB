33be23f39d418866339ad0c79dad27953b522471
Sergey Rimsha <Sergey.rimsha.ru@gmail.com>
2021-02-18 22:19:35 +0300

    Lesson 101

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -3,10 +3,9 @@ import {Col, Row, Container} from 'reactstrap';
 import Header from '../header';
 import ErrorMessage from '../errorMessage';
 import RandomChar from '../randomChar';
-import CharacterPage from '../characterPage';
-import CharDetails from '../charDetails';
-import ItemList from '../itemList';
+import {CharacterPage, BooksPage, HousesPage, BooksItem} from '../pages';
 import gotService from '../../services/gotService';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
 
 export default class App extends Component {
 
@@ -33,34 +32,31 @@ export default class App extends Component {
         }
 
         return(
-            <> 
-                <Container>
-                    <Header /> 
-                </Container>
-                <Container>
-                    <Row>
-                        <Col lg={{size: 5, offset: 0}}>
-                            {char}
-                            <button onClick={this.onShow}
-                            className="btn btn-secondary">
-                                Toggle random character
-                            </button>
-                        </Col>
-                    </Row>
-                    <CharacterPage />
-                    <Row>
-                        <Col md='6'>
-                            <ItemList 
-                            onItemSelected={this.onItemSelected}
-                            getData={this.gotService.getAllBooks}
-                            renderItem={(item) => item.name}/>
-                        </Col>
-                        <Col md='6'>
-                            <CharDetails charId={this.state.selectedChar}/>
-                        </Col>
-                    </Row> 
-                </Container>
-            </>
+            <Router>
+                <div className='app'> 
+                    <Container>
+                        <Header /> 
+                    </Container>
+                    <Container>
+                        <Row>
+                            <Col lg={{size: 5, offset: 0}}>
+                                {char}
+                                <button onClick={this.onShow}
+                                className="btn btn-secondary">
+                                    Toggle random character
+                                </button>
+                            </Col>
+                        </Row>
+                        <Route path='/' component={() => <h1>Welcome to GOT DB</h1>} exact/>
+                        <Route path='/characters' component={CharacterPage} />
+                        <Route path='/books' component={BooksPage} exact/>
+                        <Route path='/books/:id' render={({match}) => {
+                            const {id} = match.params;
+                        return <BooksItem bookId={id}/>}}/>
+                        <Route path='/houses' component={HousesPage} />
+                    </Container>
+                </div>
+            </Router>
         );
     }
 }
